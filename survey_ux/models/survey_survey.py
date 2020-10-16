4bbb5d44e5ba8b23066f18d9496736d7f21630cc
Jorge Obiols <jorge.obiols@gmail.com>
2020-10-16 14:17:08 -0300

    [FIX] hacer que la encuesta vuelva con la url de la company

--- survey_ux/models/survey_survey.py
+++ survey_ux/models/survey_survey.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from werkzeug import urls
 from odoo import api, models, fields
 
 
@@ -20,3 +21,12 @@ class Survey(models.Model):
     def _compute_code_field(self):
         for rec in self:
             rec.code_field = "EN{:05}".format(rec.id)
+
+    def _compute_survey_url(self):
+        """ Overrides the original method in order to take into account the
+            multicompany. Computes a public URL for the survey
+        """
+        #base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        base_url = self.env.company.company_registry
+        for survey in self:
+            survey.public_url = urls.url_join(base_url, "survey/start/%s" % (survey.access_token))
---
+++ survey_ux/readme/CONFIGURE.rst
@@ -0,0 +1,2 @@
+For the survey to return with the correct url it is necessary to put that url in the
+field Company Registry of the company.
